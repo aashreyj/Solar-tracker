c7b52ea58ecfeab62217ea73ba8a644cfbdb0fb7
Aashrey <aashrey9.jain@gmail.com>
2019-01-05 14:43:35 +0530

    Final changes in location

--- tracker.py
+++ tracker.py
@@ -4,38 +4,39 @@ import ephem
 import time
 import serial
 
-ser1=serial.Serial('',9600)
-ser2=serial.Serial('',9600)
+#ser1=serial.Serial('',9600) #Enter arduino port
+#ser2=serial.Serial('',9600) #Enter arduino port
 
 while True:
     utc = datetime.now(tzutc())
 
     data=ephem.Observer()
     
-    data.lon='75.3172596'
-    data.lat='19.864609599999998'
-    data.elevation=567
+    data.lon='87.28854'
+    data.lat='23.54799'
+    data.elevation=84
+    pi=3.141593
     
     data.date=str(utc)
     values=ephem.Sun(data)
 
     a=float(repr(values.alt)) 
-    pi=3.141593
     al_servo=(a*180)/pi
-    if(al_servo<0):
+    if(al_servo<=0):
         al_servo=0
 
     c=float(repr(values.az))
     d=(c*180)/pi
-    if(d<90 and d>270):
+    if(d<90 and d>270) or al_servo<0:
         azi_servo=0
     else:
         azi_servo=(d-90)
 
-    print("Altitude (upwards from horizon) = %d" %int(al_servo))
-    print("Azimuth (clock-wise from North) = %d" %int(azi_servo))
-    print(" ")
-    ser1.write(al_servo)
-    ser2.write(azi_servo)
-    time.sleep(5)
+    print("Altitude (upwards from Horizon) = %d" %int(al_servo))
+    print("Azimuth (clock-wise from East) = %d\n" %int(azi_servo))
+
+    #ser1.write(al_servo.encode())
+    #ser2.write(azi_servo.encode())
+
+    time.sleep(10)
 
